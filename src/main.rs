fdaff2ac07ffa26d5c91058b2f56db9fc8772d03
dim4egster <dim4egster@gmail.com>
2025-05-15 20:03:47 +0800

    sentry: with inherit filter

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,18 @@ fn main() {
 
 fn init_tracing() {
 
+    /*
+    // only specified ti=ype without inheritance (f.e. INFO)
     let sentry_layer = sentry_tracing::layer().event_filter(|md| match md.level() {
-        &tracing::Level::INFO => EventFilter::Event,
+        &tracing::Level::ERROR | &tracing::Level::INFO => EventFilter::Event,
         _ => EventFilter::Ignore,
-    });
-
+    });*/
+    
+    let sentry_layer = sentry_tracing::layer().with_filter(tracing_subscriber::filter::LevelFilter::INFO);
     let fmt_layer = tracing_subscriber::fmt::layer::<tracing_subscriber::Registry>()
         .with_filter(tracing_subscriber::filter::LevelFilter::INFO); // Только INFO и выше
-
-    tracing_subscriber::registry()
-        .with(fmt_layer)
-        .with(sentry_layer)
-        .init();
-
+    
+    
     // Init sentry with tracing feature
     let sentry_dsn = std::env::var("SENTRY_DSN").expect("SENTRY_DSN must be set");
     println!("SENTRY_DSN: {}", sentry_dsn);
@@ -35,5 +34,11 @@ fn init_tracing() {
             ..Default::default()
         },
     ));
-    tracing::info!("thank you god: info 9 in init_tracing");
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(sentry_layer)
+        .init();
+    
+    tracing::error!("thank you god: info 12 in init_tracing");
 }
